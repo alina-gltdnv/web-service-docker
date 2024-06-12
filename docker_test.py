c3813c58876ae828dee7acea3526c096a9152795
Alina Galiautdinova <alina1994.1336@gmail.com>
2024-06-12 14:30:45 +0200

    web service added

---
+++ docker_test.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+
+    response = {
+        "message": "Request received",
+    }
+    return jsonify(response), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
