c51a14bce2d03719473aa234eb93096a2d4bf7f1
alina-gltdnv <alina1994.1336@gmail.com>
2024-06-21 12:33:44 +0200

    added calculation of target heart rate

--- docker_test.py
+++ docker_test.py
@@ -7,20 +7,34 @@ app = Flask(__name__)
 REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
 
 
-@app.route('/submit', methods=['POST'])
+@app.route('/calculations/target-heart-rate', methods=['POST'])
 @REQUEST_TIME.time()
-def submit():
+def thr_service():
     data = request.get_json()
     if not data:
         return jsonify({"error": "No data provided"}), 400
 
+    age = data.get('age')
+    if not isinstance(age, int) or age < 18 or age > 99:
+        return jsonify({"error": "Enter an age greater than 17 and less than 100"}), 400
+
+    lower, upper = thr_counter(age)
     response = {
-        "message": "Request received",
+        "targetHeartRate": {
+            "lowerBound": lower,
+            "upperBound": upper
+        }
     }
     return jsonify(response), 200
+
+
+def thr_counter(age):
+    max_hr = 220 - age
+    lower_bound = 0.5 * max_hr
+    upper_bound = 0.85 * max_hr
+    return lower_bound, upper_bound
 
 
 if __name__ == '__main__':
     start_http_server(8333)
     app.run(debug=False, host='0.0.0.0', port=5000)
-
