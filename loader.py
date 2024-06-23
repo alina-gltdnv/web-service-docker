3ef3218bb6e7fc11b8facb30bc01f596fafe9c43
alina-gltdnv <alina1994.1336@gmail.com>
2024-06-23 16:34:53 +0200

    updated loader.py

--- loader.py
+++ loader.py
@@ -1,20 +1,23 @@
 import requests
 import time
 import random
+import json
 
 
 def target_heart_rate():
     age = random.randint(a=1, b=100)
-    url = "0.0.0.0:5000/calculations/target-heart-rate"
+    url = "http://localhost:5000/calculations/target-heart-rate"
     header = {"Content-Type": "application/json"}
-    data = {"age": age }
-    resp = requests.post(url=url, headers=header, data=data)
+    data = {"age": age}
+    resp = requests.post(url=url, headers=header, data=json.dumps(data))
+    return data, resp.text, resp.elapsed.total_seconds()
 
 
 if __name__ == '__main__':
     try:
         while True:
-            target_heart_rate()
-            time.sleep(1)
+            req, resp, dur = target_heart_rate()
+            print(f"req {req}, resp {resp}, dur {dur}")
+            time.sleep(0.5)
     except Exception as e:
         print(e)
